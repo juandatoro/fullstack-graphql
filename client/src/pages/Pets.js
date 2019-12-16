d114918a3113932897d9fd3e37ad215ddb2df28b
Juan David Restrepo <juan.restrepo@globant.com>
2019-12-16 16:20:21 -0500

    Added optimistic response

--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -5,15 +5,66 @@ import PetsList from '../components/PetsList'
 import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      id
+      name
+      type
+      img
+    }
+  }
+`
+
+const NEW_PET = gql`
+  mutation CreatePet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      id
+      name
+      type
+      img
+    }
+  }
+`
 
 export default function Pets () {
   const [modal, setModal] = useState(false)
-
+  const { data, loading, error } = useQuery(ALL_PETS)
+  const [createPet, newPet] = useMutation(NEW_PET,{
+    update(cache, {data: {addPet}}){
+      const data = cache.readQuery({query: ALL_PETS})
+      cache.writeQuery({
+        query: ALL_PETS,
+        data: { pets: [addPet, ...data.pets]}
+      })
+    }
+  })
 
   const onSubmit = input => {
+    createPet({
+      variables: {newPet: input},
+      optimisticResponse: {
+        __typename: 'Mutation',
+        addPet: {
+          __typename: 'Pet',
+          id: Math.floor(Math.random()*10000) + '',
+          name: input.name,
+          type: input.type,
+          img: 'https://via.placeholder.com/300'
+        }
+      }
+    })
     setModal(false)
   }
-  
+
+  if (loading) {
+    return <Loader />
+  }
+
+  if (error || newPet.error) {
+    return <h1>Error !</h1>
+  }
+
   if (modal) {
     return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
   }
@@ -32,7 +83,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets}/>
       </section>
     </div>
   )
